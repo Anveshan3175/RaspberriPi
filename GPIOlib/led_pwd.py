84d7aa5179571756cfc04e3c95fece3fcc41b5ae
Anveshan <akunduru4@gmail.com>
2020-10-02 20:54:07 +0530

    Create led_pwd.py

---
+++ GPIOlib/led_pwd.py
@@ -0,0 +1,17 @@
+from gpiozero import PWMLED
+from time import sleep
+
+
+led = PWMLED(17)
+
+
+while True:
+    led.value = 0 # off
+    sleep(1)
+    led.value = 0.5 # half brightness
+    sleep(1)
+    led.value = 1 # full brightness
+    sleep(1)
+
+
+''' connect BCM-17 to LED positive and -ve to Rapsberri GND [Board pin 3]  '''
