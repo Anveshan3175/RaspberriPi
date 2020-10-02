c6dcf478fc6ed81beff0d990b88b0c7ab8c660b3
Anveshan <akunduru4@gmail.com>
2020-10-02 20:36:55 +0530

    Update led_1.py

--- GPIOlib/led_1.py
+++ GPIOlib/led_1.py
@@ -1,1 +1,16 @@
 
+from gpiozero import LED
+from time import sleep
+
+red = LED(17)
+
+
+while True:
+    red.on()
+    sleep(1)
+    red.off()
+    sleep(1)
+
+
+''' connect BCM-17 to LED positive and -ve to Rapsberri GND [Board pin 3]  '''
+
