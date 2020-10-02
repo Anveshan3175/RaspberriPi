b21e4f7f40479b691ca1b1035340afb821f8807d
Anveshan <akunduru4@gmail.com>
2020-10-02 21:45:15 +0530

    Create button_press_function.py

---
+++ GPIOlib/button_press_function.py
@@ -0,0 +1,12 @@
+from gpiozero import Button
+from signal import pause
+
+def say_hello():
+    print("Hello !! Button is pressed ")
+
+
+button = Button(2)  # connect +Ve terminal of button to GPIO2 and -Ve of button to GND of raspberri 
+
+button.when_pressed = say_hello
+
+pause()
