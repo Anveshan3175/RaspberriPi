44b140f3d981ae3925f97dbf50399db9d1191425
Anveshan <akunduru4@gmail.com>
2020-11-07 23:52:18 +0530

    Create buggyrobot_remote.py

--- robots/buggyrobot_remote.py
+++ robots/buggyrobot_remote.py
@@ -1,1 +1,41 @@
 
+from gpiozero import Robot
+from bluedot import BlueDot
+
+
+bd = BlueDot()
+bd.wait_for_press()
+print("you have pressed a blue dot")
+##robot = Robot(left=(9,10), right=(7,8))  custom made
+##robot = Robot(left=(9,10),right=(8,7))  first purchase
+robot = Robot(left=(7,8),right=(10,9))  ##second purchase
+
+
+
+
+
+def move(pos):
+    if pos.top:
+        robot.forward()
+    elif pos.bottom:
+        robot.backward()
+    elif pos.right:
+        robot.right()
+    elif pos.left:
+        robot.left()
+
+def stop():
+    robot.stop()
+
+
+bd.when_pressed = move
+bd.when_moved = move
+bd.when_released = stop
+
+
+
+
+
+
+        
+        
